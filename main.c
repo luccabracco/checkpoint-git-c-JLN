3f62248639bda41265ea4683be6e505d0a5135cc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:33:17 -0300

    RM 573037

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 intmain(){
     printf("Lucca Bracco") , ("21 Anos") , ("RM 570175");
     printf("Nickollas Korner Alves") , ("18 Anos") , ("RM 569655");
-    printf("João Pedro Ferrari Faian") , ("17 Anos");
+    printf("João Pedro Ferrari Faian") , ("17 Anos") , ("RM 573037");
     return 0;
 }
