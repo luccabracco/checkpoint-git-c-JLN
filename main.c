c90076f557d36012ddd85d3c0b83eb70b460faa3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:32:31 -0300

    Idade João Pedro Ferrari Faian

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 intmain(){
     printf("Lucca Bracco") , ("21 Anos") , ("RM 570175");
     printf("Nickollas Korner Alves") , ("18 Anos") , ("RM 569655");
-    printf("João Pedro Ferrari Faian");
+    printf("João Pedro Ferrari Faian") , ("17 Anos");
     return 0;
 }
